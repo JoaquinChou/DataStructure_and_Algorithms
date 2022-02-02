6bf9c3f625baa25c6c68dee9a0339eb0eca79570
JoaquinChou <1486840262@qq.com>
2022-02-02 21:43:47 +0800

    Create brute_force_match.cpp

---
+++ DataStructure/串/朴素的模式匹配算法/brute_force_match.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+// 从主串`S`的第pos个元素开始匹配
+int bruteForceMatch(string S, string T, int pos) {
+    int i = pos;
+    int j = 0;
+    while (i < S.size() && j < T.size()) {
+        if (S[i] == T[j]) {
+            i++;
+            j++;
+        }
+        else {
+            // i又从上一次匹配的下一个位置开始
+            i = i - j + 2; 
+            j = 0;
+        }
+    }
+
+    if (j > T.size() - 1)   return i - T.size();
+    else return -1;
+
+}
+int main() {
+
+    string S = "sawtsafvda";
+    string T = "safv";
+    int res = bruteForceMatch(S, T, 0);
+    if (res >= 0) {
+        cout << "从主串的第" << (res + 1) << "个位置开始" << endl;
+    }
+    else    cout << "匹配失败" << endl;
+
+    return 0;
+}
